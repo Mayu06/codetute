1b1a6d721b3dd5fe644ff537b349f6415f4c12e8
Mayuri <mayuripeiris@gmail.com>
2019-06-27 15:42:02 +0530

    Today commit

--- application/controllers/User_detail.php
+++ application/controllers/User_detail.php
@@ -7,6 +7,14 @@ class User_detail extends CI_Controller
 	
 	function index()
 	{
+// Set the libraries
+		//$this->load->library('form_validation');
+		//$this->form_validation->set_rule();
+
+// Set the helpers
+		//$this->load->helper('email');
+		
+
 		$this->load->model('user_model');
 		$showdata['userArray'] = $this->user_model->userapplicant();
 		//var_dump($showdata);
